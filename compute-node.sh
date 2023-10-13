4289d170f557cbedac55fe514a66cc0e4dab35d4
Ragashree <ragashreeshekar@gmail.com>
2023-10-13 11:42:48 -0700

    Commands to install k8s

---
+++ compute-node.sh
@@ -0,0 +1,11 @@
+sudo apt upgdate
+sudo apt upgrade
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-get install kubeadm kubelet kubectl -y
+sudo apt-mark hold kubeadm kubelet kubectl
+sudo swapoff -a
+sudo hostnamectl set-hostname worker-node-01
---
+++ control-plane.sh
@@ -0,0 +1,16 @@
+sudo apt upgdate
+sudo apt upgrade
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-get install kubeadm kubelet kubectl -y
+sudo apt-mark hold kubeadm kubelet kubectl
+sudo swapoff -a
+sudo hostnamectl set-hostname master-node
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml`
