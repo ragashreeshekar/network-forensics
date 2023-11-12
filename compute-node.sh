d4801a2258dc0a3888395cb2ef18d784a48cebde
Ragashree <ragashreeshekar@gmail.com>
2023-11-12 11:34:29 -0800

    Update compute-node.sh

--- compute-node.sh
+++ compute-node.sh
@@ -3,6 +3,7 @@ sudo apt upgrade
 sudo apt install docker.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo apt install curl -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt-get install kubeadm kubelet kubectl -y
